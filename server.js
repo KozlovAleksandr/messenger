fc1fcd6e7b99741d68de3a21a851e5b4187fce3e
KozlovAleksandr <aleksandr.an.kozlov@gmail.com>
2023-01-30 22:32:36 +0300

    server fix

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
 const express = require("express");
+const path = require("path");
 
 const app = express();
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
 app.use(express.static("dist"));
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Сервер запущен по адресу http://localhost:${port}`);
